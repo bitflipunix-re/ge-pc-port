6b6f4bc459f72dbdce748e77a04686e5c031625a
bitflipunix-re <bitflipunix@atomicmail.io>
2026-09-27 08:18:47 +1000

    compiler: harden mpmenu.c

--- work/goldeneye-pc-port/src/game/mpmenu.c
+++ work/goldeneye-pc-port/src/game/mpmenu.c
@@ -970,29 +970,29 @@ void write_playerrank_to_buffer(char *buffer, s32 playernum)
     switch (j)
     {
         case 0:
-            sprintf(buffer, langGet(getStringID(LMPMENU, MPMENU_STR_11_RANK1ST))); /* Rank: 1st */
+            strcpy(buffer, langGet(getStringID(LMPMENU, MPMENU_STR_11_RANK1ST))); /* Rank: 1st */
             break;
         case 1:
-            sprintf(buffer, langGet(getStringID(LMPMENU, MPMENU_STR_12_RANK2ND))); /* Rank: 2nd */
+            strcpy(buffer, langGet(getStringID(LMPMENU, MPMENU_STR_12_RANK2ND))); /* Rank: 2nd */
             break;
         case 2:
             if ((scenario != SCENARIO_2v2) && (scenario != SCENARIO_2v1))
             {
-                sprintf(buffer, langGet(getStringID(LMPMENU, MPMENU_STR_13_RANK3RD))); /* Rank: 3rd */
+                strcpy(buffer, langGet(getStringID(LMPMENU, MPMENU_STR_13_RANK3RD))); /* Rank: 3rd */
             }
             else
             {
-                sprintf(buffer, langGet(getStringID(LMPMENU, MPMENU_STR_12_RANK2ND))); /* Rank: 2nd */
+                strcpy(buffer, langGet(getStringID(LMPMENU, MPMENU_STR_12_RANK2ND))); /* Rank: 2nd */
             }
             break;
         case 3:
             if (scenario != SCENARIO_3v1)
             {
-                sprintf(buffer, langGet(getStringID(LMPMENU, MPMENU_STR_14_RANK4TH))); /* Rank: 4th */
+                strcpy(buffer, langGet(getStringID(LMPMENU, MPMENU_STR_14_RANK4TH))); /* Rank: 4th */
             }
             else
             {
-                sprintf(buffer, langGet(getStringID(LMPMENU, MPMENU_STR_12_RANK2ND))); /* Rank: 2nd */
+                strcpy(buffer, langGet(getStringID(LMPMENU, MPMENU_STR_12_RANK2ND))); /* Rank: 2nd */
             }
             break;
     }
