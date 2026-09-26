26855c305d4ca673871b090b0ee366229337ab82
bitflipunix-re <bitflipunix@atomicmail.io>
2026-09-26 18:00:00 +1000

    arm64: compare animation pointers at host width

--- work/goldeneye-pc-port/src/game/initanitable.h
+++ work/goldeneye-pc-port/src/game/initanitable.h
@@ -32,12 +32,15 @@ extern struct animation_table_data* ptr_animation_table;
 #include <stdint.h>
 #define GE_ANIMDATA_OFFSET(name) ((u32)PTR_ANIM_##name)
 #define GE_ANIMDATA_BASE ((uintptr_t)&ptr_animation_table->data)
+#define GE_ANIMDATA_RUNTIME_ADDR(ptr) ((uintptr_t)(ptr))
 #else
 #define GE_ANIMDATA_OFFSET(name) ((s32)&ANIM_DATA_##name)
 #define GE_ANIMDATA_BASE ((s32)&ptr_animation_table->data)
+#define GE_ANIMDATA_RUNTIME_ADDR(ptr) ((s32)(ptr))
 #endif
 #define GE_ANIMDATA_ADDR(name) (GE_ANIMDATA_BASE + GE_ANIMDATA_OFFSET(name))
 #define GE_ANIMDATA_PTR(name) ((void *)GE_ANIMDATA_ADDR(name))
+#define GE_ANIMDATA_MATCH(ptr, name) (GE_ANIMDATA_RUNTIME_ADDR(ptr) == GE_ANIMDATA_ADDR(name))
 
 /**
  * Contains offsets into ptr_animation_table for player and guard animations.
